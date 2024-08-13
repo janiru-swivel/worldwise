a5410f391830761d579436925861f95109c74f9f
janiru-swivel <janiru@swivelgroup.com.au>
2024-08-13 12:56:33 +0530

    commit 3 - until the using css modules (209.)

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -1,7 +1,26 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayOut from "./pages/AppLayOut";
+
 function App() {
   const x = 23;
 
-  return <div>WorldWise</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="Product" element={<Product />} />
+          <Route path="Pricing" element={<Pricing />} />
+          <Route path="app" element={<AppLayOut />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
